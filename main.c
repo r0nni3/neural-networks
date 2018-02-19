5f186d19e49c17235b5391af652377508df7937b
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2018-02-19 01:33:11 -0400

    Fixes code style

--- main.c
+++ main.c
@@ -39,7 +39,8 @@ struct NNNode {
         double *weight;
 };
 
-void init_node (struct NNNode *node, int input_size) {
+void init_node (struct NNNode *node, int input_size)
+{
         node->value = 0.0;
         node->bias = 0.0;
         node->input_size = input_size;
@@ -47,7 +48,8 @@ void init_node (struct NNNode *node, int input_size) {
         node->weight = calloc(input_size, sizeof(double));
 }
 
-void free_node (struct NNNode *node) {
+void free_node (struct NNNode *node)
+{
         free(node->in);
         free(node->weight);
 }
@@ -75,28 +77,29 @@ void init_layer (struct NNLayer *layer, int size, int node_input_size, char *nam
 
 void free_layer (struct NNLayer *layer)
 {
-        for(int i = 0; i < layer->size; ++i) {
+        for (int i = 0; i < layer->size; ++i) {
                 free_node(&layer->nodes[i]);
         }
         free(layer->nodes);
         free(layer->name);
 }
 
-void print_layer (struct NNLayer *layer) {
+void print_layer (struct NNLayer *layer)
+{
         printf("%-11s %-12s\n", "=== Layer:", layer->name);
-        for(int i = 0; i < layer->size; ++i) {
+        for (int i = 0; i < layer->size; ++i) {
                 printf("%-11s=== Node %02d\n", "", i+1);
                 printf("%-15s%-9s %d\n", "", "Size:", layer->nodes[i].input_size);
                 printf("%-15s%-9s %f\n", "", "Value:", layer->nodes[i].value);
                 printf("%-15s%-9s %f\n", "", "Bias:", layer->nodes[i].bias);
                 printf("%-15s%-9s %s", "", "IN:", "[");
-                for(int j=0; j < layer->nodes[i].input_size; ++j) {
+                for (int j = 0; j < layer->nodes[i].input_size; ++j) {
                         printf("%f", layer->nodes[i].in[j]);
                         if (j != layer->nodes[i].input_size -1) printf(", ");
                         else printf("]\n");
                 }
                 printf("%-15s%-9s %s", "", "WEIGHT:", "[");
-                for(int j=0; j < layer->nodes[i].input_size; ++j) {
+                for (int j = 0; j < layer->nodes[i].input_size; ++j) {
                         printf("%f", layer->nodes[i].weight[j]);
                         if (j != layer->nodes[i].input_size -1) printf(", ");
                         else printf("]\n");
@@ -106,7 +109,8 @@ void print_layer (struct NNLayer *layer) {
         printf("\n");
 }
 
-void activate_layer (struct NNLayer *current, struct NNLayer *next) {
+void activate_layer (struct NNLayer *current, struct NNLayer *next)
+{
         for (int i = 0; i < next->size; ++i) {
                 double sum = next->nodes[i].bias;
                 for (int j = 0; j < current->size; ++j) {
@@ -126,10 +130,10 @@ void init_input_layer (struct NNLayer *layer)
         // Generic layer initialization
         init_layer(layer, INPUT_LAYER_SIZE, INPUT_LAYER_NODE_INPUT_SIZE, "Input");
         // First layer initialization
-        for(int i = 0; i < layer->size; ++i) {
+        for (int i = 0; i < layer->size; ++i) {
                 layer->nodes[i].value = 1.0 * ((rand() % INPUT_LAYER_SIZE) + 1);
                 layer->nodes[i].bias = 0.1 * ((rand() % INPUT_LAYER_SIZE) +1);
-                for(int j = 0; j < layer->nodes[i].input_size; ++j) {
+                for (int j = 0; j < layer->nodes[i].input_size; ++j) {
                         layer->nodes[i].in[j] = layer->nodes[i].value;
                         layer->nodes[i].weight[j] = 1.0;
                 }
@@ -148,8 +152,8 @@ void init_hidden_layer (struct NNLayer *layer)
 {
         init_layer(layer, HIDDEN_LAYER_SIZE, HIDDEN_LAYER_NODE_INPUT_SIZE, "Hidden");
         // First layer initialization
-        for(int i = 0; i < layer->size; ++i) {
-                for(int j = 0; j < layer->nodes[i].input_size; ++j) {
+        for (int i = 0; i < layer->size; ++i) {
+                for (int j = 0; j < layer->nodes[i].input_size; ++j) {
                         double b = rand() % (HIDDEN_LAYER_NODE_INPUT_SIZE);
                         double w = 0.1 * (b + 0.01);
                         layer->nodes[i].weight[j] = w;
@@ -168,8 +172,8 @@ void free_hidden_layer (struct NNLayer *layer)
 void init_output_layer (struct NNLayer *layer)
 {
         init_layer(layer, OUTPUT_LAYER_SIZE, OUTPUT_LAYER_NODE_INPUT_SIZE, "Output");
-        for(int i = 0; i < layer->size; ++i) {
-                for(int j = 0; j < layer->nodes[i].input_size; ++j) {
+        for (int i = 0; i < layer->size; ++i) {
+                for (int j = 0; j < layer->nodes[i].input_size; ++j) {
                         double b = rand() % (OUTPUT_LAYER_NODE_INPUT_SIZE);
                         double w = 0.1 * (b + 0.01);
                         layer->nodes[i].weight[j] = w;
